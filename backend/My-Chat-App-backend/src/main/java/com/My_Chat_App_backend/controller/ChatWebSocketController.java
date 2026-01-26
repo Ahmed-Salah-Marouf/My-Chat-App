f850b12af1b6adab64e90649ef2a57e0c2538d11
Ahmed Salah <elshitehi1@gmail.com>
2026-01-26 19:30:34 +0200

    fix: WebSocket sender and add CORS config

--- backend/My-Chat-App-backend/src/main/java/com/My_Chat_App_backend/controller/ChatWebSocketController.java
+++ backend/My-Chat-App-backend/src/main/java/com/My_Chat_App_backend/controller/ChatWebSocketController.java
@@ -3,8 +3,10 @@ package com.My_Chat_App_backend.controller;
 import com.My_Chat_App_backend.dto.ChatMessageSendDto;
 import com.My_Chat_App_backend.dto.MessageDto;
 import com.My_Chat_App_backend.entity.Message;
+import com.My_Chat_App_backend.entity.User;
 import com.My_Chat_App_backend.service.ChatRoomService;
 import com.My_Chat_App_backend.service.MessageService;
+import com.My_Chat_App_backend.service.UserService;
 import lombok.AllArgsConstructor;
 import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
@@ -17,14 +19,18 @@ import org.springframework.stereotype.Controller;
 public class ChatWebSocketController {
     private final ChatRoomService chatRoomService;
     private final MessageService messageService;
+    private final UserService userService;
 
     @MessageMapping("/sendMessage/{chatRoomId}")
     @SendTo("/topic/chatroom.{chatRoomId}")
     public MessageDto sendMessage(@Payload ChatMessageSendDto chatMessageSendDto
             , @DestinationVariable Long chatRoomId) {
 
+        User sender = userService.getUserEntityById(chatMessageSendDto.getSenderId());
+
         Message message = Message.builder()
                 .content(chatMessageSendDto.getContent())
+                .sender(sender)
                 .build();
         Message savedMessage = chatRoomService.createMessageInChatRoom(chatRoomId, message);
 
